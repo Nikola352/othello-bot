040d672c28d6b1e902209d3269d706a7046b2bb1
Nikola Jolovic <nikolajolovic02@gmail.com>
2025-08-19 16:45:16 +0200

    Add conversion from checkpoint to policy net only file

--- ai/main.py
+++ ai/main.py
@@ -1,3 +1,5 @@
+import torch
+from ai.model.agent import DqnAgent
 from train.process_games import prepare_dataset
 from train.supervised import train_supervised
 from train.rl import train_rl
@@ -7,6 +9,13 @@ def main():
     states, actions = prepare_dataset("../data/othello_dataset.csv")
     network = train_supervised(states, actions, save_path="../output/supervised/network.pth")
     train_rl("../output/rl/model.pth", policy_net=network, checkpoint_dir="../output/rl/checkpoints")
+
+
+def convert():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    agent = DqnAgent(device)
+    agent.load_model("../output/rl/checkpoints/checkpoint_10000.pth")
+    agent.save_policy_net("../output/rl/network_10000.pth")
 
 
 if __name__ == "__main__":
